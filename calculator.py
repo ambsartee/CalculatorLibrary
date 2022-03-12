d67bff05869546d6d84d66386b3ce7c5ceed0322
Ambrose Sartee <ambsartee@yahoo.com>
2022-03-12 08:34:11 -0500

    updated calculator py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
@@ -10,3 +11,5 @@ def subtract(first_term, second_term):
     return first_term - second_term
 
 
+def multiply(first_term, second_term):
+    return first_term * second_term
